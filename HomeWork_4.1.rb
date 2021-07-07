00edafa0aa696b6494bd52cf08c6f9f92f7233b6
Aleksandr Bogdanov <qxn105@mail.ru>
2021-07-08 07:59:56 +0800

    Задача 4-1 Вывести большее из чисел

---
+++ HomeWork_4.1.rb
@@ -0,0 +1,12 @@
+﻿var1 = 172.169
+var2 = 31.514
+
+if (var1==var2)
+	puts "Числа равны"
+else
+	if (var1>var2)
+		print "Наибольшее число:", var1 
+	else                              
+		print "Наибольшее число:", var2 
+	end
+end
--- objects.rb
+++
@@ -1,5 +0,0 @@
-﻿puts "я объект типа строка!".length
-
-puts "17.to_f=",17.to_f,endl=nil
-
-puts "17.to_r=",17.to_r
