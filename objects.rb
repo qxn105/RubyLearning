d3200073412f0d1339281b03d6ab978830a14e2e
Aleksandr Bogdanov <qxn105@mail.ru>
2021-07-08 07:58:58 +0800

    Урок 4: Объекты, переменные и конструкция “IF”

---
+++ objects.rb
@@ -0,0 +1,5 @@
+﻿puts "я объект типа строка!".length
+
+puts "17.to_f=",17.to_f,endl=nil
+
+puts "17.to_r=",17.to_r
---
+++ variables.rb
@@ -0,0 +1,11 @@
+﻿hello1= "Привет, пацаны!"
+hello2= "Привет, девчонки!"
+girls = nil
+
+if (girls != nil)
+	puts hello1
+else
+	puts hello2
+end
+
+puts "До свидания!"
