275f5a8e1864c0d1bd551827010742081f8276ec
Aleksandr Bogdanov <qxn105@mail.ru>
2021-07-08 08:09:31 +0800

    Урок 6 Массивы и поток выполнения

---
+++ arrays.rb
@@ -0,0 +1,33 @@
+﻿fruits = ["apple", "banana", "orange"]
+puts fruits.class.name
+
+puts
+puts fruits[0]
+
+puts
+puts fruits
+
+puts
+puts fruits.to_s
+
+basket = []
+basket << "fruit1"
+basket << "fruit2" << "fruit3"
+puts
+puts basket.to_s
+
+basket.push("cherry", "mango")
+puts
+puts basket.to_s
+
+basket << "fruit2"
+puts
+puts basket.to_s
+
+basket.delete("fruit2")
+puts
+puts basket.to_s
+
+basket.delete_at(0)
+puts
+puts basket.to_s
