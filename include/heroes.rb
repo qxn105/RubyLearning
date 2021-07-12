3c97feabbc010d55a97111633e6e1bf70ab9d6a4
Aleksandr Bogdanov <qxn105@mail.ru>
2021-07-12 11:29:59 +0800

    Задача 12-1 Герои и злодеи (case-when)

---
+++ include/heroes.rb
@@ -0,0 +1,30 @@
+﻿# Для игры реализуем два класса: Game - для логики игры
+
+class Heroes
+
+	def initialize
+		@heroes = ["Бэтмен", "Шерлок Холмс", "Буратино", "Фродо Бэггинс", "Моцарт"]
+	end
+
+	def help
+		return @heroes.join(", ")
+	end
+
+	def antihero(hero)
+	  case hero
+		when "бэтмен", "batman", "batmеn"
+			return "Джокер"
+		when "шерлок холмс", "sherlock holmes"
+			return "Профессор Мориарти"
+		when "буратино", "buratino"
+			 return "Карабас-Барабас"
+		when "фродо бэггинс", "frodo baggins"
+			return "Саурон"
+		when "моцарт", "mozzart"
+			return "Сальери"
+		else 
+			return "Нет такой информации."
+		end
+	end
+
+end
