a49360d41eaf0feb15fe2bf6f2158983399c70f0
Aleksandr Bogdanov <qxn105@mail.ru>
2021-07-12 10:13:05 +0800

    Урок 12 Объектная «Виселица» v.2

---
+++ include/result_printer.rb
@@ -0,0 +1,161 @@
+﻿# Для игры реализуем два класса: Game - для логики игры, ResultPrinter - для интерфейса
+
+class ResultPrinter
+
+	# очистить экран
+	def cls
+		system "clear" or system "cls"
+	end
+
+	def print_status(game)
+	  # puts "good_letters_out: #{game.good_letters.sort.to_s}"
+	  # puts "letters: #{game.letters.sort.to_s}"
+    if game.status == -1
+      puts "Вы проиграли :("
+      puts "Загаданное слово было: " + game.letters_out.join("")
+			STDIN.gets
+    elsif game.status == 1
+      puts "Поздравляем, вы выиграли!"
+      puts "Загаданное слово было: " + game.letters_out.join("")
+			STDIN.gets
+    else
+			puts "Слово: #{get_word_for_print(game.letters_out, game.good_letters)}"
+		  puts "Ошибки: #{game.bad_letters.join(", ").to_s}"	
+      puts "У вас осталось ошибок: " + (7 - game.errors).to_s
+    end	
+	end
+
+	# возвращает слово для вывода (отгаданые буквы видны / неотгаданные скрыты прочерками)
+	def get_word_for_print(letters_out, good_letters)
+	  result = ""
+    for item in letters_out do
+      if good_letters.include?(item)
+        result += item + " "
+      else
+        result += "__ "
+      end
+    end
+    return result
+  end
+
+	def print_viselitsa(errors)
+		cls
+  	case errors
+	  when 0
+      puts "
+            _______
+            |/
+            |
+            |
+            |
+            |
+            |
+            |
+            |
+          __|________
+          |         |
+          "
+     when 1
+      puts "
+            _______
+            |/
+            |     ( )
+            |
+            |
+            |
+            |
+            |
+            |
+          __|________
+          |         |
+          "
+    when 2
+      puts "
+            _______
+            |/
+            |     ( )
+            |      |
+            |
+            |
+            |
+            |
+            |
+          __|________
+          |         |
+          "
+    when 3
+      puts "
+            _______
+            |/
+            |     ( )
+            |      |_
+            |        \\
+            |
+            |
+            |
+            |
+          __|________
+          |         |
+          "
+    when 4
+      puts "
+            _______
+            |/
+            |     ( )
+            |     _|_
+            |    /   \\
+            |
+            |
+            |
+            |
+          __|________
+          |         |
+          "
+    when 5
+      puts "
+            _______
+            |/
+            |     ( )
+            |     _|_
+            |    / | \\
+            |      |
+            |
+            |
+            |
+          __|________
+          |         |
+          "
+  
+    when 6
+      puts "
+            _______
+            |/
+            |     ( )
+            |     _|_
+            |    / | \\
+            |      |
+            |     / \\
+            |    /   \\
+            |
+          __|________
+          |         |
+          "
+    when 7
+      puts "
+            _______
+            |/     |
+            |     (_)
+            |     _|_
+            |    / | \\
+            |      |
+            |     / \\
+            |    /   \\
+            |
+          __|________
+          |         |
+          "
+  
+    end
+  end
+end
+
---
+++ viselitsa.rb
@@ -0,0 +1,28 @@
+﻿# Виселица Е и Ё/И и Й
+# В программе «Виселица» есть важный недостаток: она не считает буквы е и ё одной буквой. А следовало бы.
+
+# Было бы гораздо удобнее, если бы она принимала эти буквы за одну и открывала заодно 
+# и все буквы ё, если введена е и наоборот. Аналогично с буквами и и й.
+
+# Исправьте программу так, чтобы реализовать эту возможность.
+
+require "./include/game.rb"
+require "./include/result_printer.rb"
+
+
+slovo = ARGV[0]
+if(Gem.win_platform? && ARGV[0])
+	slovo = slovo.encode(ARGV[0].encoding, Encoding.default_external).encode("UTF-8")
+end
+
+game = Game.new(slovo)
+printer = ResultPrinter.new
+
+while game.status == 0 do
+  printer.print_viselitsa(game.errors)
+	printer.print_status(game)
+	game.ask_next_letter
+end
+printer.print_viselitsa(game.errors)
+printer.print_status(game)
+
