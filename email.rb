e9d582522c0d933b1996e6a7074a074aebf63b47
Aleksandr Bogdanov <qxn105@mail.ru>
2021-07-14 12:23:34 +0800

    Задача 18-1 Проверяем email

---
+++ email.rb
@@ -0,0 +1,35 @@
+﻿# Проверяем email
+# Напишите программу, которая проверяет является ли введенный текст email-ом.
+
+# Например:
+
+# $ ruby email.rb 
+# Введите email:
+# install.vv@gmail.com
+# Спасибо!
+
+# $ ruby email.rb 
+# Введите email:
+# Какая-то фигня!
+# Это не email
+
+# Этот код необходим только при использовании русских букв на Windows
+if Gem.win_platform?
+  Encoding.default_external = Encoding.find(Encoding.locale_charmap)
+  Encoding.default_internal = __ENCODING__
+
+  [STDIN, STDOUT].each do |io|
+    io.set_encoding(Encoding.default_external, Encoding.default_internal)
+  end
+end
+
+puts "Введите email:"
+str = gets.chomp.encode("UTF-8")
+
+result = str =~ /^[\d\w\.]+@[\d\w\.]+$/
+if result
+	puts "Спасибо!"
+else
+	puts "Какая-то фигня!"
+	puts "Это не email"
+end
