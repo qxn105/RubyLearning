209572c0ca6a5a5419668786d05bcf3f7d7d08c3
Aleksandr Bogdanov <qxn105@mail.ru>
2021-07-15 18:02:59 +0800

    Задача 23-4 Область видимости в классах

---
+++ Lesson 23/Exercise 4/scopes.rb
@@ -0,0 +1,39 @@
+﻿# переменная $a - глобальная
+# переменная b - локальная
+# переменная c - внутри метода
+class MyClass 
+	@variable_0
+
+	def initialize
+		@variable = 0
+	end
+
+	def check_variables
+		puts "\nВнутри объекта класса:"
+		puts "Переменная '@variable_0' #{defined?(@variable_0) ? "" : "не "}определена"	
+		puts "Переменная '@variable' #{defined?(@variable) ? "" : "не "}определена"	
+		puts "Переменная '$a' #{defined?($a) ? "" : "не "}определена"
+		puts "Переменная 'b' #{defined?(b) ? "" : "не "}определена"	
+		puts "Переменная 'с' #{defined?(c) ? "" : "не "}определена"	
+	end
+end
+
+$a = 0
+b = 0
+
+def method
+	c = 0
+	puts "\nВнутри метода:"
+	puts "Переменная '$a' #{defined?($a) ? "" : "не "}определена"
+	puts "Переменная 'b' #{defined?(b) ? "" : "не "}определена"	
+	puts "Переменная 'с' #{defined?(c) ? "" : "не "}определена"	
+end
+
+method
+puts "\nЛокально:"
+puts "Переменная '$a' #{defined?($a) ? "" : "не "}определена"
+puts "Переменная 'b' #{defined?(b) ? "" : "не "}определена"	
+puts "Переменная 'с' #{defined?(c) ? "" : "не "}определена"	
+
+class_obj = MyClass.new
+class_obj.check_variables
