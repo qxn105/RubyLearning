a37bb8dd691cf11529ef899409f7c951fbb725c3
Aleksandr Bogdanov <qxn105@mail.ru>
2021-07-21 15:27:28 +0800

    Задача 34-1 Тесты на метод hashtags

---
+++ Lesson 34/Exercise 1/spec/hashtags_spec.rb
@@ -0,0 +1,38 @@
+﻿require 'hashtags.rb'
+
+describe 'Hashtag' do
+  it 'Захватывается 1 хэштег' do
+    expect(hashtags('words with #tags')).to eq %w(#tags)
+	end
+
+  it 'Захватывается несколько хэштегов' do
+    expect(hashtags('words with #tags and #end')).to eq %w(#tags #end)
+	end
+
+  it 'Захватывается хэштег на русском' do
+    expect(hashtags('words with #tags, #end, #луна')).to eq %w(#tags #end #луна)
+	end
+
+  it 'Захватывается хэштег с подчеркиваниями' do
+    expect(hashtags('words with #tags, #end, #луна. #первый_класс')).to eq %w(#tags #end #луна #первый_класс)
+	end
+
+  it 'Захватывается хэштег с минусами' do
+    expect(hashtags('words with #tags, #end, #луна. #первый_класс #one-two')).to eq %w(#tags #end #луна #первый_класс #one-two)
+	end
+
+  it 'Не захватывается знак вопроса' do
+    expect(hashtags('words with #tags, #end, #луна. #первый_класс #one-two?')).to eq %w(#tags #end #луна #первый_класс #one-two)
+	end
+
+  it 'Не захватывается восклицательный знак' do
+    expect(hashtags('words with #tags, #end, #луна. #первый_класс #one-two!')).to eq %w(#tags #end #луна #первый_класс #one-two)
+	end
+
+  it 'Не захватывается точка с запятой' do
+    expect(hashtags('words with #tags, #end, #луна. #первый_класс; #one-two!')).to eq %w(#tags #end #луна #первый_класс #one-two)
+	end
+end
+
+
+
